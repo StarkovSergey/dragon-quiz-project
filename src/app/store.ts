2a4b91eaf7baec358cf58344d0110a3b9b0a8f01
korolevskiy08 <emkorolev12@gmail.com>
2022-09-19 00:47:49 +0300

    feat: merge signUpReducer in authReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,14 +4,12 @@ import { AnyAction, combineReducers } from 'redux'
 import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
 import { authReducer } from '../features/auth/auth-reducer'
-import { signUpReducer } from '../features/auth/sign-up/signUp-reducer'
 
 import { appReducer } from './app-reducer'
 
 const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
-  signUp: signUpReducer,
 })
 
 export const store = configureStore({
