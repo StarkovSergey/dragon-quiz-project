34fb6cae98d28a179bf4ed4e155a65ba2d88909a
Starkov Sergey <starkovsr@gmail.com>
2022-09-21 18:06:17 +0300

    feat: add styles for check-email page

--- src/features/auth/check-email/CheckEmail.tsx
+++ src/features/auth/check-email/CheckEmail.tsx
@@ -1,3 +1,28 @@
+import React from 'react'
+
+import { NavLink } from 'react-router-dom'
+
+import mailImg from '../../../assets/images/mail.svg'
+import { Button } from '../../../common/components/Button/Button'
+import { InputText } from '../../../common/components/InputText/InputText'
+import authStyle from '../auth.module.css'
+import style from '../sign-in/SignIn.module.css'
+
 export const CheckEmail = () => {
-  return <h1>CheckEmail</h1>
+  return (
+    <div className={authStyle.container}>
+      <h1 className="section-title">Check Email</h1>
+      <div className={authStyle['input-box']}>
+        <img src={mailImg} alt="" />
+      </div>
+
+      <div className={authStyle['button-box']}>
+        <p className={authStyle.text}>We’ve sent an Email with instructions to example@mail.com</p>
+
+        <NavLink className="link" to={'/sign-in'}>
+          Back to login
+        </NavLink>
+      </div>
+    </div>
+  )
 }
